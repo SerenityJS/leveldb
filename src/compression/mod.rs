8f6032cf9a1d407fd994bcc5ee9ba44b3756b248
Payton Kerby <paytonmk744@outlook.com>
2026-01-01 22:29:37 -0600

    chore: format code

--- src/compression/mod.rs
+++ src/compression/mod.rs
@@ -1,5 +1,5 @@
-mod zlib_compressor;
 mod raw_zlib_compressor;
+mod zlib_compressor;
 
-pub use zlib_compressor::ZlibCompressor;
 pub use raw_zlib_compressor::RawZlibCompressor;
+pub use zlib_compressor::ZlibCompressor;
